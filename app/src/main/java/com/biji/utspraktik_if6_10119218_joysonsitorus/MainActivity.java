5498c3b992d042b33bd2a7d038f6925588010cea
bijiman <joyss234@gmail.com>
2022-06-03 16:03:14 +0700

    Initial commit

---
+++ app/src/main/java/com/biji/utspraktik_if6_10119218_joysonsitorus/MainActivity.java
@@ -0,0 +1,14 @@
+package com.biji.utspraktik_if6_10119218_joysonsitorus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
